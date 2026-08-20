d2fc5384304db8d8d716639f5603825c14fa911a
Manthan0678 <manthan0678@gmail.com>
2026-08-20 10:42:43 +0530

    Adding learnt split function

--- using_hash().py
+++ using_hash().py
@@ -1,4 +1,4 @@
 if __name__ == '__main__':
-    n = int(raw_input())
-    integer_list = tuple(map(int, raw_input().split()))
+    n = int(input())
+    integer_list = tuple(map(int, input().split()))
     print(hash(integer_list))
